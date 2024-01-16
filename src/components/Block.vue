fa7f43665128c6bbc42f70498e900bd1292ba708
Elvis Zhune <elfabri28@gmail.com>
2024-01-16 12:33:30 +0000

    Proyecto Vue

---
+++ src/components/Block.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="block" v-if="showBlock">
+        Click Me
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false
+        }
+    },
+    mounted() {
+        console.log('component mounted')
+        setTimeout(() => {
+            this.showBlock = true
+            console.log(this.delay)
+        }, this.delay)
+    },
+    updated() {
+
+    }
+}
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        background-color: aquamarine;
+        color: white;
+    }
+
+
+</style>
+
+
