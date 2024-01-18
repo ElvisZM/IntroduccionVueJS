27913e886210d52aca2ca1b6490181a880babffb
Elvis Zhune <elfabri28@gmail.com>
2024-01-18 08:47:04 +0000

    vue-ninja

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
-      this.delay = 2000 + Math.random() * 5000
+      this.delayPadre = 2000 + Math.random() * 5000
       this.isPlaying = true
-      console.log(this.delay)
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.isPlaying = false
+      this.showResults = true
+      this.score = reactionTime
+      document.getElementById('play_btn').innerText = 'Play Again'
     }
   }
 }
@@ -29,8 +38,11 @@ export default {
 
   <main>
     <h1>Ninja Reaction Timer</h1>
-    <button @click="start" :disabled="isPlaying">Play</button>
-    <Block v-if="isPlaying" :delay="delay"/>
+    <div>
+      <button @click="start" id="play_btn" :disabled="isPlaying">Play</button>
+    </div>
+    <Block v-if="isPlaying" :delay="delayPadre" @end="endGame"/>
+    <Results v-if="showResults" :score="score"/>
   </main>
 </template>
 
@@ -42,6 +54,20 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+h1 {
+  border-bottom: 1px solid #ccc;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+button {
+  margin: auto 45%;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
 }
 
 </style>
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,46 +1,57 @@
 <template>
     <div class="block" v-if="showBlock" @click="stopTimer">
-        Click Me
+        Click Me!
     </div>
 </template>
 
 <script>
-export default {
-    props: ['delay'],
-    data() {
-        return {
-            showBlock: false,
-            timer: null,
-            reactionTime: 0
-        }
-    },
-    mounted() {
-        console.log('component mounted')
-        setTimeout(() => {
-            this.showBlock = true
-            this.startTimer()
-            console.log(this.delay)
-        }, this.delay)
-    },
-    methods: {
-        startTimer() {
-            this.timer = setInterval(() => {
-                this.reactionTime += 10
-            }, 10)
+    export default {
+        props: ['delay'],
+        data() {
+            return {
+                showBlock: false,
+                timer: null,
+                reactionTime: 0
+            }
         },
-        stopTimer() {
-            clearInterval(this.timer)
+        mounted() {
+            //console.log('mounted')
+            setTimeout(() => {
+                this.showBlock = true, this.startTimer()
+            }, this.delay)
+        },
+        updated(){
+            //console.log('updated')
+        },
+        unmounted() {
+            //console.log('unmounted')
+        },
+        methods: {
+            startTimer() {
+                this.timer = setInterval(() => {
+                    this.reactionTime += 10
+                }, 10)
+            },
+            stopTimer() {
+                clearInterval(this.timer)
+                this.showBlock = false
+                this.$emit('end', this.reactionTime)
+            }
         }
     }
-}
 
 </script>
 
 <style>
     .block {
-        width: 400px;
+        display: inline-block;
+        width: 35%;
         background-color: aquamarine;
         color: white;
+        margin: 15px 40%;
+        border-radius: 10px;
+        font-size: 1.5rem;
+        
     }
 
 
